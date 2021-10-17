bcea3ba09627c1421a0a80a35c0fd936742ab8f1
Virginia Lopez Nadal <vlopeznadal@icloud.com>
2021-10-17 13:41:32 -0500

    Completed Part 2: Methods

--- oo.py
+++ oo.py
@@ -9,7 +9,23 @@ class Question(object):
         self.question = question
         self.correct_answer = correct_answer
 
+    def ask_and_evaluate(self):
+        answer = input(self.question + " > ")
+        return self.correct_answer == answer
+
 class Exam(object):
     def __init__(self, name):
         self.name = name
         self.questions = []
+
+    def add_question(self, question):
+        self.questions.append(question)
+
+    def administer(self):
+        answer_tally = 0
+
+        for question in self.questions:
+            if question.ask_and_evaluate():
+                answer_tally += 1
+                
+        return 100 * (float(answer_tally) / len(self.questions))
