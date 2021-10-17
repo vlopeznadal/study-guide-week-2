ffa4115ae6bc34b5c7070d686c1e3e52d0c63627
Virginia Lopez Nadal <vlopeznadal@icloud.com>
2021-10-17 12:42:29 -0500

    Completed Part 1: Classes and Init Methods

--- oo.py
+++ oo.py
@@ -1,1 +1,15 @@
-# Create your classes here
+class Student(object):
+    def __init__(self, first_name, last_name, address):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+
+class Question(object):
+    def __init__(self, question, correct_answer):
+        self.question = question
+        self.correct_answer = correct_answer
+
+class Exam(object):
+    def __init__(self, name):
+        self.name = name
+        self.questions = []
